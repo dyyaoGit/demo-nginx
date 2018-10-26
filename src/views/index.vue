5e66476627a706e20c56bc15d7032522023b9e7e
dyyao <646617513@qq.com>
2018-10-26 10:05:21 +0800

    pi

---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>我是第一页</h1>
+
+      <router-link to="second">跳转到第二页</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/second.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>我是第二页</h1>
+
+
+      <router-link to="/">跳转回首页</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "second"
+    }
+</script>
+
+<style scoped>
+
+</style>
